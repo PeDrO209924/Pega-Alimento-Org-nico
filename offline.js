e1de47b5094bafe846452aa8acc4db3d211239bf
PeDrO209924 <74063252+PeDrO209924@users.noreply.github.com>
2020-11-06 12:40:00 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1604671890,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/cesta-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/l-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite3-sheet0.png",
+		"images/atiradorbom-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/sprite4-sheet0.png",
+		"images/balamau-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-128.png",
+		"icon-256.png"
+	]
+}
